35dd85d9150a105f203139b0dd5ec6fb090377d0
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-02-17 12:37:31 +0530

    Navbar completed

---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavbarLogo = () => {
+  return (
+    <div className='pr-12'>
+
+        <h1 className=' text-white text-2xl sm:hidden md:block'>
+          Sujeet Kumar Gupta</h1>
+
+        <h1 className=' text-white font-special font-extrabold 
+        text-4xl sm:block md:hidden'>SG</h1>
+    </div>
+  )
+}
+
+export default NavbarLogo
