5ceb07bb4c87224d28240e7cba5df0dc316f602d
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-04-30 12:13:33 +0530

    skills section updated

--- src/components/skillsSection/AllSkills.jsx
+++ src/components/skillsSection/AllSkills.jsx
@@ -1,39 +1,38 @@
 import React from 'react'
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
+import {FaReact, FaNodeJs, FaGitAlt, FaAws } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { SiRedux } from "react-icons/si";
-import { SiNextdotjs } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
+import {SiExpress, SiAppwrite } from "react-icons/si";
 
 import SingleSkill from "./SingleSkill";
 
 const skills = [
     {
-      skill: "HTML",
-      icon: FaHtml5,
-    },
-    {
-      skill: "CSS",
-      icon: FaCss3Alt,
-    },
-    {
       skill: "JavaScript",
       icon: IoLogoJavascript,
     },
     {
-      skill: "ReactJS",
+      skill: "Node",
+      icon: FaNodeJs,
+    },
+    {
+      skill: "Express",
+      icon: SiExpress,
+    },
+    {
+      skill: "Appwrite",
+      icon: SiAppwrite,
+    },
+    {
+      skill: "Git",
+      icon: FaGitAlt,
+    },
+    {
+      skill: "AWS",
+      icon: FaAws,
+    },
+    {
+      skill: "React",
       icon: FaReact,
-    },
-    {
-      skill: "Redux",
-      icon: SiRedux,
-    },
-    {
-      skill: "TailwindCSS",
-      icon: RiTailwindCssFill,
     },
   ];
 
--- src/components/skillsSection/AllSkillsSM.jsx
+++ src/components/skillsSection/AllSkillsSM.jsx
@@ -1,43 +1,38 @@
 import React from 'react'
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
+import {FaReact, FaNodeJs, FaGitAlt, FaAws } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { SiRedux } from "react-icons/si";
-import { SiNextdotjs } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
+import {SiExpress, SiAppwrite } from "react-icons/si";
 
 const skills = [
-  {
-    skill: "HTML",
-    icon: FaHtml5,
-  },
-  {
-    skill: "CSS",
-    icon: FaCss3Alt,
-  },
-  {
-    skill: "JavaScript",
-    icon: IoLogoJavascript,
-  },
-  {
-    skill: "ReactJS",
-    icon: FaReact,
-  },
-  {
-    skill: "Redux",
-    icon: SiRedux,
-  },
-  {
-    skill: "NextJS",
-    icon: SiNextdotjs,
-  },
-  {
-    skill: "TailwindCSS",
-    icon: RiTailwindCssFill,
-  },
-];
+    {
+      skill: "JavaScript",
+      icon: IoLogoJavascript,
+    },
+    {
+      skill: "Node",
+      icon: FaNodeJs,
+    },
+    {
+      skill: "Express",
+      icon: SiExpress,
+    },
+    {
+      skill: "Appwrite",
+      icon: SiAppwrite,
+    },
+    {
+      skill: "Git",
+      icon: FaGitAlt,
+    },
+    {
+      skill: "AWS",
+      icon: FaAws,
+    },
+    {
+      skill: "React",
+      icon: FaReact,
+    },
+  ];
 
 const AllSkillsSM = () => {
   return (
