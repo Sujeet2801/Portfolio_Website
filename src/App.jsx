451a185bf12faf47964b07d451a21abfa0f96870
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-06-26 10:48:13 +0530

    feat: Add certificates route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarMain from "./components/navbar/NavbarMain";
 import Home_Page from "./Home_Page";
-import Project_Page from "./Project_Page";
+import Project_Page from "./page/project/Project_Page";
+import CertificatePage from "./page/certificate/CertificatePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home_Page />} />
           <Route path="/projects" element={<Project_Page />} />
+          <Route path="/certificates" element={<CertificatePage />} />
         </Routes>
       </main>
     </Router>
