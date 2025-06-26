b8712c954048083b62a5ba7e93279042ff976530
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-06-26 10:47:27 +0530

    Add clodinary image url

--- src/components/aboutMeSection.jsx/AboutMeImage.jsx
+++ src/components/aboutMeSection.jsx/AboutMeImage.jsx
@@ -5,7 +5,7 @@ const AboutMeImage = () => {
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src="./images/about-me.jpg"
+          src="https://res.cloudinary.com/dw6mns230/image/upload/v1750825184/about-me_xwudd6.jpg"
           alt="About Me Image"
           className="h-full w-auto object-cover"
         />
